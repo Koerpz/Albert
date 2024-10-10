19cd3b04b8e95067b06ab20e039f1d777ebf2c57
”vinvis3006” <vincent.paulusma@student.nhlstenden.nl>
2024-10-10 12:22:33 +0200

    De knoppen hebben nu namen

--- Leo/Window2.xaml.cs
+++ Leo/Window2.xaml.cs
@@ -30,5 +30,12 @@ namespace Leo
             Window1.Show();
             this.Close();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
     }
 }
