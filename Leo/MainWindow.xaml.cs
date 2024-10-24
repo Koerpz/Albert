213c427a224129d085a6ee3185c9608429ed5dde
”vinvis3006” <vincent.paulusma@student.nhlstenden.nl>
2024-10-24 21:42:46 +0200

    achtergrond maar nu echt

--- Leo/MainWindow.xaml.cs
+++ Leo/MainWindow.xaml.cs
@@ -19,6 +19,13 @@ namespace Leo
         public MainWindow()
         {
             InitializeComponent();
+
+            // Achtergrond instellen naar een afbeelding vanuit de resources
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Assets/achtergrond.jpg"));
+
+            // Stel de achtergrond van het huidige Window in
+            this.Background = brush;  // 'this' verwijst naar het huidige venster
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
